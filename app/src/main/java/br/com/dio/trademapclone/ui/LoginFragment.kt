0713d3312da977220f75f05a80c8bbe2d7c5d0b5
joao-freitas-nexaas <master.007,1>
2020-12-13 16:35:53 -0300

    init project

--- app/src/main/java/br/com/dio/trademapclone/ui/LoginFragment.kt
+++ app/src/main/java/br/com/dio/trademapclone/ui/LoginFragment.kt
@@ -3,15 +3,12 @@ package br.com.dio.trademapclone.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import br.com.dio.trademapclone.MainActivity
 import br.com.dio.trademapclone.R
-import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        (activity as MainActivity).toolbar.visibility = View.GONE
     }
 
 }
